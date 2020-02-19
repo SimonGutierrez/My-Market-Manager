8c6de9d7b0c66c16a41ce6ab81b076794e1aaa63
SimonG <saguti15@g.holycross.edu>
2020-02-19 18:26:27 -0500

    feat: attempting to connect app to firebase

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import fireBaseConfig from '../../secrets';
 
 const config = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_DOMAIN_HERE',
-  databaseURL: 'YOUR_DB_URL',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: fireBaseConfig.apiKey,
+  authDomain: fireBaseConfig.authDomain,
+  databaseURL: fireBaseConfig.databaseURL,
+  projectId: fireBaseConfig.projectId,
+  storageBucket: fireBaseConfig.storageBucket,
+  messagingSenderId: fireBaseConfig.messagingSenderId,
+  appId: fireBaseConfig.appId,
+  measurementId: fireBaseConfig.measurementId,
 };
 
 firebase.initializeApp(config);
