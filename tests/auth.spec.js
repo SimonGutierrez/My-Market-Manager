db38391b30a5e65ddcb2bf309aa2efd7ce8be4ec
SimonG <saguti15@g.holycross.edu>
2020-05-08 21:29:33 -0400

    added more tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,30 +1,8 @@
 import { expect } from 'chai';
-import { getFirebase } from 'react-redux-firebase';
-import { getFirestore } from 'redux-firestore';
-import { signUpThunkCreator } from '../src/store/reducers/authReducer';
-
-
 
 const test = () => 'This is a test';
 
-describe('sign up', async () => {
-  const fireBase = getFirebase();
-  const firestore = getFirestore();
-
-  const newUser = {
-    email: 'newUser@gmail.com',
-    password: 'Password1',
-    firstName: 'Newuser',
-    lastName: 'Test'
-  }
-
-  signUpThunkCreator(newUser);
-
-  const getUser = await fireBase
-                          .auth()
-                          .getUserByEmail('newUser@gmail.com')
-
-
+describe('sign up', () => {
 
     it('requires an email', () => {
       expect(test()).to.equal('This is a test');
